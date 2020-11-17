41bf055f56eb9f3fdc2e0c3022a5b1704921f120
Conor Doyle <cdoyle4@firstderivatives.com>
2020-11-17 12:35:47 +0000

    7# : Unit-tests for basic connection initialisation and authentication

---
+++ kx.Test/TestUtils/TestSerialisationHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace kx.Test.TestUtils
+{
+    /// <summary>
+    /// A helper class for testing serialisation and de-serialisation logic 
+    /// in unit-tests
+    /// </summary>
+    internal static class TestSerialisationHelper
+    {
+        /// <summary>
+        /// Performs binary serialisation and de-serialisation on a specified exception.
+        /// </summary>
+        /// <typeparam name="T">The type of exception being tested.</typeparam>
+        /// <param name="exception">The exception to be serialised and de-serialised.</param>
+        /// <returns>
+        /// A de-serialised instance of the exception passed. All serialisable members should match the original.
+        /// </returns>
+        /// <remarks>
+        /// This is primarily intended to confirm custom exceptions within the DeltaApiCore 
+        /// library comply to ISerialization pattern.
+        /// 
+        /// See https://stackoverflow.com/questions/94488/what-is-the-correct-way-to-make-a-custom-net-exception-serializable
+        /// </remarks>
+        public static T SerialiseAndDeserialiseException<T>(T exception)
+            where T : Exception
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            using (var stream = new MemoryStream())
+            {
+                binaryFormatter.Serialize(stream, exception);
+
+                stream.Seek(0, 0);
+
+                return (T)binaryFormatter.Deserialize(stream);
+            }
+        }
+    }
+}
---
+++ kx.Test/TestUtils/TestableTcpServer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace kx.Test.TestUtils
+{
+    /// <summary>
+    /// A wrapper over a TCP Listener to simulate a running KDB+ environment for which
+    /// client log-in will succeed.
+    /// </summary>
+    internal sealed class TestableTcpServer : IDisposable
+    {
+        private readonly TcpListener _server;
+
+        /// <summary>
+        /// Initialises a new default instance of <see cref="TestableTcpServer"/>.
+        /// </summary>
+        public TestableTcpServer()
+        {
+            _server = new TcpListener(IPAddress.Loopback, 0);
+            _server.Start();
+
+            //kick off task to simulate successful log-on
+            Task.Factory.StartNew(() =>
+            {
+                //block until client has established connection
+                if (!_server.Pending())
+                {
+                    System.Threading.Thread.Sleep(10);
+                }
+
+                //now get the stream from the client
+                var client = _server.AcceptTcpClient();
+                var stream = client.GetStream();
+
+                byte[] buffer = new byte[client.ReceiveBufferSize];
+
+                // read incoming stream
+                stream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                //simulate authentication succeeding
+                stream.Write(buffer, 0, 1);
+            });
+        }
+
+        /// <summary>
+        /// Gets the port this class is connected to for testing.
+        /// </summary>
+        public int TestPort
+        {
+            get
+            {
+                return ((IPEndPoint)_server.LocalEndpoint).Port;
+            }
+        }
+
+        #region IDisposable Members
+        public void Dispose()
+        {
+            if (_server != null)
+            {
+                _server.Stop();
+            }
+        }
+        #endregion IDisposable Members
+    }
+}
