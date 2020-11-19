31c187d82f595587166dec8c58a4e56d68d1106a
Conor Doyle <cdoyle4@firstderivatives.com>
2020-11-19 17:34:41 +0000

    Ensure check against release builds

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -2,8 +2,8 @@
 echo "Executing MSBuild DLL begin command..."
 dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"cdoyle-kx" /k:"cdoyle-kx_csharpkdb" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.cs.opencover.reportsPaths="*/coverage.opencover.xml" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=false /d:sonar.login=${SONAR_TOKEN}
 echo "Running build..."
-dotnet build ./CSharpKdb.sln
+dotnet build /p:Configuration=Release ./CSharpKdb.sln
 echo "Running tests..."
-dotnet test ./kx.Test/kx.Test.csproj --logger:trx /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet test /p:Configuration=Release --no-build ./kx.Test/kx.Test.csproj --logger:trx /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 echo "Executing MSBuild DLL end command..."
 dotnet ./tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
