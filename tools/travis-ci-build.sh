cbde1f6b7b69f8b1679b5b26420a9c0edc06097a
cdoyle-kx <74536948+cdoyle-kx@users.noreply.github.com>
2020-11-20 17:23:28 +0000

    Only run Sonar-Scan if not Pull-Request (#29)

--- tools/travis-ci-build.sh
+++ tools/travis-ci-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Only Run SonarScanner if it is not a pull_request
-if ["$TRAVIS_PULL_REQUEST" == "false"]; then
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	echo "Executing MSBuild DLL begin command..."
 	dotnet ./tools/sonar/SonarScanner.MSBuild.dll begin /o:"kxsystems" /k:"KxSystems_csharpkdb" /d:sonar.cs.vstest.reportsPaths="**/TestResults/*.trx" /d:sonar.cs.opencover.reportsPaths="*/coverage.opencover.xml" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.verbose=false /d:sonar.login=${SONAR_TOKEN}
 fi
@@ -13,7 +13,7 @@ dotnet build /p:Configuration=Release ./CSharpKdb.sln
 echo "Running tests..."
 dotnet test /p:Configuration=Release --no-build ./kx.Test/kx.Test.csproj --logger:trx /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 
-if ["$TRAVIS_PULL_REQUEST" == "false"]; then
-echo "Executing MSBuild DLL end command..."
-dotnet ./tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
+if ["$TRAVIS_PULL_REQUEST" = "false"]; then
+	echo "Executing MSBuild DLL end command..."
+	dotnet ./tools/sonar/SonarScanner.MSBuild.dll end /d:sonar.login=${SONAR_TOKEN}
 fi
