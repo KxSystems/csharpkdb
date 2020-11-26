6f9acc15803e24e866383cee25d5d2f7b763a0db
cdoyle-kx <74536948+cdoyle-kx@users.noreply.github.com>
2020-11-26 10:12:37 +0000

    Add examples of api use. Fixes issue #40 (#41)

---
+++ Demos/SerializationDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using kx;
+using NLog;
+
+namespace SerializationDemo
+{
+    static class Program
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main()
+        {
+            string host = "localhost";
+            int port = 5001;
+            string usernamePassword = $"{Environment.UserName}:mypassword";
+
+            c connection = null;
+
+            int[] input = CreateTestArray(i => i % 10, 50000);
+
+            try
+            {
+                connection = new c(host, port, usernamePassword);
+
+                int[] deserialisedResult =
+                    connection.Deserialize(connection.Serialize(1, input)) as int[];
+
+                Logger.Info("{0}", System.Linq.Enumerable.SequenceEqual(input, deserialisedResult));
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Error occurred running Serialization-Demo. \r\n{ex}");
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        private static T[] CreateTestArray<T>(Func<int, T> elementBuilder, int arraySize)
+        {
+            T[] array = new T[arraySize];
+
+            for (int i = 0; i < arraySize; i++)
+            {
+                array[i] = elementBuilder(i);
+            }
+            return array;
+        }
+    }
+}
---
+++ Demos/SubscriberDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using kx;
+using NLog;
+
+namespace SubscriberDemo
+{
+    static class Program
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        static void Main()
+        {
+            string host = "localhost";
+            int port = 5001;
+            string usernamePassword = $"{Environment.UserName}:mypassword";
+
+            c connection = null;
+            try
+            {
+                connection = new c(host, port, usernamePassword);
+
+                //open subscription
+                connection.ks(".u.sub", "mytable", "MSFT");
+
+                bool subscribing = true;
+
+                //start processing subscriptions until user exit or error
+                Task.Factory.StartNew(() =>
+                {
+                    Logger.Info("Processing subscription results. Press any key to exit");
+                    while (subscribing)
+                    {
+                        try
+                        {
+                            Logger.Info($"Received subscription result:{connection.k()}");
+                        }
+                        catch (Exception)
+                        {
+                            Logger.Error("Error occurred processing Subscription. Exiting Subscription-Demo");
+                            subscribing = false;
+                        }
+                    }
+                });
+
+                Console.ReadLine();
+                subscribing = false;
+
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"Error occurred running Subscription-Demo. \r\n{ex}");
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
