eaa7f1715e77783f58ddf9743afaa34135f8d62b
Simon Shanks <59612559+sshanks-kx@users.noreply.github.com>
2025-03-19 09:54:09 +0000

    running java installs as root (#63)

--- tools/travis-ci-install-sonar.sh
+++ tools/travis-ci-install-sonar.sh
@@ -1,13 +1,15 @@
   
 #!/bin/sh
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-    echo "Updating Java for Ubuntu 16.04.7"
-    apt-get update
-    apt-get install -y software-properties-common
-    add-apt-repository -y ppa:linuxuprising/java
-    apt-get update
-    echo oracle-java17-installer shared/accepted-oracle-license-v1-3 select true | /usr/bin/debconf-set-selections
-    apt install -y oracle-java17-installer --install-recommends
+    echo "Sonar requires updated Java for Ubuntu 16.04.7, updating repos"
+    sudo apt-get update
+    sudo apt-get install -y software-properties-common
+    sudo add-apt-repository -y ppa:linuxuprising/java
+    sudo apt-get update
+    echo "Set properties to agree to Java17 license prompts"
+    sudo echo oracle-java17-installer shared/accepted-oracle-license-v1-3 select true | /usr/bin/debconf-set-selections
+    echo "Install Java17"
+    sudo apt install -y oracle-java17-installer --install-recommends
 fi
 echo "Starting sonar install..."
 wget -O sonar.zip https://github.com/SonarSource/sonar-scanner-msbuild/releases/download/5.0.4.24009/sonar-scanner-msbuild-5.0.4.24009-netcoreapp3.0.zip
